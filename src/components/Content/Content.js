896230f0d202d6bae78d498747c36df15a03b416
ilya5015 <83602272+ilya5015@users.noreply.github.com>
2022-05-07 19:29:55 +0300

    Add files via upload

---
+++ src/components/Content/Content.js
@@ -0,0 +1,30 @@
+import anime from './anime.jpg'
+
+const Content = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src={ anime }/>
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                my posts
+            <div>
+                new post
+            </div>
+            <div>
+            <div>
+                post1
+            </div>
+            <div>
+                post2
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Content
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import logo from './logo.png'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src={ logo }/>
+        </header>
+    )
+}
+
+export default Header
