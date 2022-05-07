7898ea70fcfd4c047179a983450a21a2361af772
ilya5015 <83602272+ilya5015@users.noreply.github.com>
2022-05-07 19:29:55 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header/Header.js'
+import Navbar from './components/Navbar/Navbar'
+import Content from './components/Content/Content'
+
+
+function App() {
+  return (
+    <div className='app-wrapper'>
+      <Header />
+      <Navbar />
+      <Content />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+
+
+const Navbar = () => {
+    return (
+        <nav className='nav'>
+            <div>
+                Profile
+            </div>
+            <div>
+                Messages
+            </div>
+            <div>
+                News
+            </div>
+            <div>
+                Music
+            </div>
+      </nav>
+    )
+}
+
+export default Navbar
