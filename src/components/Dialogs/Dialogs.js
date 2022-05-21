6bfce7e74577a87dada7bd105ba6d9e15b0ce61d
Ilya <ilya50152@yandex.ru>
2022-05-21 13:57:38 +0300

    Made components for messages, people from dialogs, posts. Implemented Array.map for rendering of posts, messages and people from dialogs components. git status! TODO: need to observe SRP principle and to carry data of post, dialogs and messages to index.js

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,63 +1,36 @@
-import { NavLink } from "react-router-dom";
 import styles from "./Dialogs.module.css";
 
-const DialogItem = (props) => {
-  let path = "/dialogs/" + props.id;
-
-  return (
-    <div className={styles.dialogItem + " " + styles.active}>
-      <NavLink to={path}>{props.name}</NavLink>
-    </div>
-  );
-};
-
-const DialogMessage = (props) => {
-  return <div className={styles.dialogMessage}>{props.message}</div>;
-};
-
-const dialogsData = [
-  { id: 1, name: "Ilya" },
-  { id: 1, name: "Alexey" },
-  { id: 1, name: "Sergey" },
-  { id: 1, name: "Artem" },
-  { id: 1, name: "Dmitriy" },
-  { id: 1, name: "Vladimir" },
-];
-
-const messageData = [
-  { id: 1, message: "hi" },
-  { id: 1, message: "how are you" },
-  { id: 1, message: "nice to meet you" },
-];
+import DialogItem from "./DialogItem/DialogItem";
+import DialogMessage from "./DialogMessage/DialogMessage";
 
 const Dialogs = (props) => {
+  const dialogsData = [
+    { id: 1, name: "Ilya" },
+    { id: 1, name: "Alexey" },
+    { id: 1, name: "Sergey" },
+    { id: 1, name: "Artem" },
+    { id: 1, name: "Dmitriy" },
+    { id: 1, name: "Vladimir" },
+  ];
+
+  let dialogsElements = dialogsData.map((dialogItem) => {
+    return <DialogItem name={dialogItem.name} id={dialogItem.id} />;
+  });
+
+  const messagesData = [
+    { id: 1, message: "hi" },
+    { id: 1, message: "how are you" },
+    { id: 1, message: "nice to meet you" },
+  ];
+
+  let messagesElements = messagesData.map((messageItem) => {
+    return <DialogMessage message={messageItem.message} />;
+  });
+
   return (
     <div className={styles.dialogs}>
-      <div className={styles.dialogsItems}>
-        <div className={styles.dialogsItem}>
-          <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-        </div>
-        <div className={styles.dialogsItem}>
-          <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-        </div>
-        <div className={styles.dialogsItem}>
-          <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-        </div>
-        <div className={styles.dialogsItem}>
-          <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-        </div>
-        <div className={styles.dialogsItem}>
-          <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
-        </div>
-        <div className={styles.dialogsItem}>
-          <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
-        </div>
-      </div>
-      <div className={styles.dialogMessages}>
-        <DialogMessage message={messageData[0].message} />
-        <DialogMessage message={messageData[1].message} />
-        <DialogMessage message={messageData[2].message} />
-      </div>
+      <div className={styles.dialogsItems}>{dialogsElements}</div>
+      <div className={styles.dialogMessages}>{messagesElements}</div>
     </div>
   );
 };
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -2,24 +2,38 @@ import styles from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+  let postData = [
+    {
+      id: 1,
+      message: "Hi there, how are you ?",
+      likeCounter: "40",
+      dislikeCounter: "20",
+    },
+    {
+      id: 2,
+      message: "It`s my first post",
+      likeCounter: "30",
+      dislikeCounter: "10",
+    },
+  ];
+
+  let postElements = postData.map((post) => {
+    return (
+      <Post
+        message={post.message}
+        likeCounter={post.likeCounter}
+        dislikeCounter={post.dislikeCounter}
+      />
+    );
+  });
+
   return (
     <div className={styles.postsBlock}>
       my posts
       <div>
         <button type="button">Add new post</button>
       </div>
-      <div className={styles.posts}>
-        <Post
-          message="Hi there, how are you ?"
-          likeCounter="20"
-          dislikeCounter="40"
-        />
-        <Post
-          message="It`s my first post"
-          likeCounter="15"
-          dislikeCounter="23"
-        />
-      </div>
+      <div className={styles.posts}>{postElements}</div>
     </div>
   );
 };
