78d6269a2d3df792baffb04ab12f788999429075
ilya5015 <ilya50152@yandex.ru>
2022-07-11 12:26:18 +0300

    Refactored code and made presentational and container components for Users page. Added gif preloader for Users page

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,114 +1,82 @@
-import axios from "axios";
-import React from "react";
 import styles from "./Users.module.css";
 
-class Users extends React.Component {
-  componentDidMount() {
-    if (this.props.users.length === 0) {
-      axios
-        .get(
-          `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
-        )
-        .then((response) => {
-          console.log(response);
-          this.props.setUsers(response.data.items);
-          this.props.setTotalUsers(response.data.totalCount);
-        });
-    }
+const Users = (props) => {
+  let pagesCount = Math.ceil(props.totalUsers / props.pageSize);
+  let pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
   }
 
-  onPageChanged = (page) => {
-    this.props.setCurrentPage(page);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`
-      )
-      .then((response) => {
-        console.log(response);
-        this.props.setUsers(response.data.items);
-      });
-  };
-
-  render() {
-    let pagesCount = Math.ceil(this.props.totalUsers / this.props.pageSize);
-    let pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-      pages.push(i);
-    }
-
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          {pages.map((page) => {
-            return (
-              <span
-                className={
-                  this.props.currentPage === page
-                    ? styles.selectedPage
-                    : styles.page
-                }
-                onClick={() => {
-                  this.onPageChanged(page);
-                }}
-              >
-                {page}
-              </span>
-            );
-          })}
-        </div>
-
-        <div>
-          {this.props.users.map((user) => (
-            <div key={user.id}>
-              <span>
-                <div>
-                  <img
-                    src={
-                      user.photos.small != null
-                        ? (user.photos.small =
-                            "https://temperaturka.com/wp-content/uploads/5/d/f/5dfafb5bc640c3978688e632a5aa46f3.jpe")
-                        : user.photos.small
-                    }
-                    width="100"
-                    height="100"
-                  />
-                </div>
-                <div>
-                  {user.followed ? (
-                    <button
-                      onClick={() => {
-                        this.props.unfollow(user.id);
-                      }}
-                    >
-                      Unfollow
-                    </button>
-                  ) : (
-                    <button
-                      onClick={() => {
-                        this.props.follow(user.id);
-                      }}
-                    >
-                      Follow
-                    </button>
-                  )}
-                </div>
-              </span>
-              <span>
-                <span>
-                  <div>{user.name}</div>
-                  <div>{user.status}</div>
-                </span>
-                <span>
-                  <div>{"user.location.city"}</div>
-                  <div>{"user.location.country"}</div>
-                </span>
-              </span>
-            </div>
-          ))}
-        </div>
+        {pages.map((page) => {
+          return (
+            <span
+              className={
+                props.currentPage === page ? styles.selectedPage : styles.page
+              }
+              onClick={() => {
+                props.onPageChanged(page);
+              }}
+            >
+              {page}
+            </span>
+          );
+        })}
       </div>
-    );
-  }
-}
+
+      <div>
+        {props.users.map((user) => (
+          <div key={user.id}>
+            <span>
+              <div>
+                <img
+                  src={
+                    user.photos.small != null
+                      ? (user.photos.small =
+                          "https://temperaturka.com/wp-content/uploads/5/d/f/5dfafb5bc640c3978688e632a5aa46f3.jpe")
+                      : user.photos.small
+                  }
+                  width="100"
+                  height="100"
+                />
+              </div>
+              <div>
+                {user.followed ? (
+                  <button
+                    onClick={() => {
+                      props.unfollow(user.id);
+                    }}
+                  >
+                    Unfollow
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      props.follow(user.id);
+                    }}
+                  >
+                    Follow
+                  </button>
+                )}
+              </div>
+            </span>
+            <span>
+              <span>
+                <div>{user.name}</div>
+                <div>{user.status}</div>
+              </span>
+              <span>
+                <div>{"user.location.city"}</div>
+                <div>{"user.location.country"}</div>
+              </span>
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Users;
