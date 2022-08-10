5111ae48553926a33e56a99aa184d8eb8974586f
ilya5015 <ilya50152@yandex.ru>
2022-08-10 12:46:58 +0300

    Used memo with functional component ProfileStatus

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.js
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.js
@@ -1,63 +1,65 @@
 import React, { useEffect, useState } from "react";
 
-const ProfileStatus = ({ userStatus, updateUserStatus, myId, currentId }) => {
-  const [statusText, setStatusText] = useState("");
-  const [editMode, setEditMode] = useState(false);
+const ProfileStatus = React.memo(
+  ({ userStatus, updateUserStatus, myId, currentId }) => {
+    const [statusText, setStatusText] = useState("");
+    const [editMode, setEditMode] = useState(false);
 
-  useEffect(() => {
-    setStatusText(() => userStatus);
-  }, [userStatus]);
+    useEffect(() => {
+      setStatusText(() => userStatus);
+    }, [userStatus]);
 
-  let setActivateEditMode = () => {
-    setEditMode(true);
-  };
+    let setActivateEditMode = () => {
+      setEditMode(true);
+    };
 
-  let setDeactivateEditMode = () => {
-    setEditMode(false);
-  };
+    let setDeactivateEditMode = () => {
+      setEditMode(false);
+    };
 
-  let updateLocalStatusText = (status) => {
-    if (myId && !currentId) {
-      setStatusText(status);
-    } else {
-      console.log("noooope");
-      console.log(myId, currentId);
-    }
-  };
+    let updateLocalStatusText = (status) => {
+      if (myId && !currentId) {
+        setStatusText(status);
+      } else {
+        console.log("noooope");
+        console.log(myId, currentId);
+      }
+    };
 
-  let updateStatusText = (status) => {
-    updateUserStatus(status);
-  };
+    let updateStatusText = (status) => {
+      updateUserStatus(status);
+    };
 
-  return (
-    <div>
-      {!editMode ? (
-        <div
-          onDoubleClick={() => {
-            if (myId && !currentId) {
-              setActivateEditMode();
-            }
-          }}
-        >
-          {statusText}
-        </div>
-      ) : (
-        <div>
-          <input
-            value={statusText}
-            autoFocus={true}
-            onBlur={() => {
-              setDeactivateEditMode();
-              updateStatusText(statusText);
+    return (
+      <div>
+        {!editMode ? (
+          <div
+            onDoubleClick={() => {
+              if (myId && !currentId) {
+                setActivateEditMode();
+              }
             }}
-            onChange={(e) => {
-              updateLocalStatusText(e.target.value);
-            }}
-          ></input>
-        </div>
-      )}
-    </div>
-  );
-};
+          >
+            {statusText}
+          </div>
+        ) : (
+          <div>
+            <input
+              value={statusText}
+              autoFocus={true}
+              onBlur={() => {
+                setDeactivateEditMode();
+                updateStatusText(statusText);
+              }}
+              onChange={(e) => {
+                updateLocalStatusText(e.target.value);
+              }}
+            ></input>
+          </div>
+        )}
+      </div>
+    );
+  }
+);
 
 export default ProfileStatus;
