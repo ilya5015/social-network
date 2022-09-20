0bcfa9bb71b0cde3b508fc6d6e306505c61f4527
ilya5015 <ilya50152@yandex.ru>
2022-09-20 17:26:48 +0300

    Started to implement online chat with Socket IO with my servers API

--- src/components/Dialogs/Chat/Chat.js
+++ src/components/Dialogs/Chat/Chat.js
@@ -1,25 +1,47 @@
-import { useAppDispatch } from "../../../hooks/hooks";
-import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../hooks/hooks";
+import { useEffect, useState } from "react";
 import {
   startMessagesListening,
   stopMessagesListening,
 } from "../../Redux/chat-reducer";
 import styles from "../Dialogs.module.css";
 import ChatMessages from "./ChatMessages/ChatMessages";
+import socketio from "socket.io-client";
+import Cookies from "js-cookie";
 
 const Chat = () => {
+  const [isAuth] = useAppSelector((state) => [state.authReducer.isAuth]);
   const dispatch = useAppDispatch();
 
+  const [messages, setMessages] = useState([]);
+
   useEffect(() => {
-    dispatch(startMessagesListening());
-    return () => {
-      stopMessagesListening();
-    };
-  }, []);
+    if (isAuth === true) {
+      console.log(Cookies.get("token"));
+      const socket = socketio.connect("http://localhost:5000/", {
+        withCredentials: true,
+        extraHeaders: {},
+      });
+      socket.on("message", (msg) => {
+        console.log("message", msg);
+        setMessages((messages) => [...messages, msg]);
+      });
+      socket.on("chatMessage", (msg) => {
+        console.log("chatMessage", msg);
+      });
+    }
+
+    return () => {};
+  }, [isAuth]);
 
   return (
     <div className={styles.chatContainer}>
-      <ChatMessages />
+      <div>
+        {messages.map((message) => (
+          <div>{JSON.stringify(message)}</div>
+        ))}
+      </div>
+      {/* <ChatMessages /> */}
     </div>
   );
 };
