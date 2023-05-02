1d9e6f50de078498a440863eb66d54d45f97aad8
unknown <ilya50152@yandex.ru>
2023-05-02 20:22:18 +0300

    Store slice for windows system (open, close, minimize, fullscreen, toFront)

--- src/components/Redux/store.ts
+++ src/components/Redux/store.ts
@@ -2,17 +2,19 @@ import authSlice from "./auth-reducer";
 import profileSlice from "./profile-reducer";
 import usersSlice from "./users-reducer";
 import appSlice from "./app-reducer";
-import boardSlice from "./board-reducer"
+import boardSlice from "./board-reducer";
+import windowsSlice from "./windows-reducer";
 import { configureStore } from "@reduxjs/toolkit";
 
 const store = configureStore({
-  reducer: 
-  {profilePage: profileSlice,
-  usersPage: usersSlice,
-  authReducer: authSlice,
-  appReducer: appSlice,
-  boardReducer: boardSlice
-  }
+  reducer: {
+    profilePage: profileSlice,
+    usersPage: usersSlice,
+    authReducer: authSlice,
+    appReducer: appSlice,
+    boardReducer: boardSlice,
+    windowsReducer: windowsSlice,
+  },
 });
 
 export default store;
---
+++ src/components/Redux/windows-reducer.ts
@@ -0,0 +1,170 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+type InitialStateType = {
+  expandedWindows: Array<any>;
+  // [
+  //   {
+  //     id: "string",
+  //     props: { type: "string", name: "string" },
+  //     clusterName: "chat|board",
+  //   },
+  // ],
+  windowsOptions: Array<any>;
+  // [
+  //   {
+  //     id: "string",
+  //     options: {
+  //       width: "number",
+  //       height: "number",
+  //       top: "number",
+  //       left: "number",
+  //       fullscreen: "bool",
+  //       minimized: "bool",
+  //     },
+  //   },
+  // ],
+};
+
+const initialState: InitialStateType = {
+  expandedWindows: [],
+  windowsOptions: [],
+};
+
+const windowsSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {
+    openWindow: (state, action: PayloadAction<any>) => {
+      const windowId = action.payload.windowId;
+
+      const expandedWindows = state.expandedWindows;
+      const windowsOptions = state.windowsOptions;
+
+      const filteredExpandedWindows = expandedWindows
+        .filter((window) => window.id !== windowId)
+        .concat({
+          id: windowId,
+          props: { ...action.payload.props },
+          clusterName: action.payload.clusterName,
+        });
+
+      const filteredWindowsOptions = windowsOptions
+        .filter((window) => window.id !== windowId)
+        .concat({
+          id: windowId,
+          options: { ...action.payload?.options } || {},
+        });
+
+      [state.expandedWindows, state.windowsOptions] = [
+        filteredExpandedWindows,
+        filteredWindowsOptions,
+      ];
+    },
+    closeWindow: (state, action: PayloadAction<any>) => {
+      const windowId = action.payload.windowId;
+
+      const expandedWindows = state.expandedWindows;
+      const windowsOptions = state.windowsOptions;
+
+      const filteredExpandedWindows = expandedWindows.filter(
+        (window) => window.id !== windowId
+      );
+
+      const filteredWindowsOptions = windowsOptions.filter(
+        (window) => window.id !== windowId
+      );
+
+      [state.expandedWindows, state.windowsOptions] = [
+        filteredExpandedWindows,
+        filteredWindowsOptions,
+      ];
+    },
+    toFrontWindow: (state, action: PayloadAction<any>) => {
+      const windowId = action.payload.windowId;
+
+      const expandedWindows = state.expandedWindows;
+      const windowsOptions = state.windowsOptions;
+
+      const filteredExpandedWindows = expandedWindows
+        .filter((window) => window.id !== windowId)
+        .concat(expandedWindows.find((window) => window.id === windowId));
+
+      const filteredWindowsOptions = windowsOptions.map((window) => {
+        if (window.id === windowId) {
+          return {
+            ...window,
+            options: {
+              ...window.options,
+              minimized: false,
+            },
+          };
+        }
+        return window;
+      });
+
+      [state.expandedWindows, state.windowsOptions] = [
+        filteredExpandedWindows,
+        filteredWindowsOptions,
+      ];
+    },
+    minimizeWindow: (state, action: PayloadAction<any>) => {
+      const windowId = action.payload.windowId;
+
+      const expandedWindows = state.expandedWindows;
+      const windowsOptions = state.windowsOptions;
+
+      const filteredExpandedWindows = [
+        expandedWindows.find((window) => window.id === windowId),
+        ...expandedWindows.filter((window) => window.id !== windowId),
+      ];
+
+      const filteredWindowsOptions = windowsOptions.map((window) => {
+        if (window.id === windowId) {
+          return {
+            ...window,
+            options: {
+              ...window.options,
+              minimized: !window.options.minimized,
+            },
+          };
+        }
+        return window;
+      });
+
+      [state.expandedWindows, state.windowsOptions] = [
+        filteredExpandedWindows,
+        filteredWindowsOptions,
+      ];
+    },
+    fullscreenWindow: (state, action: PayloadAction<any>) => {
+      const windowId = action.payload.windowId;
+
+      const windowsOptions = state.windowsOptions;
+
+      const filteredWindowsOptions = windowsOptions.map((window) => {
+        if (window.id === windowId) {
+          return {
+            ...window,
+            options: {
+              ...window.options,
+              fullscreen: !window.options.fullscreen,
+            },
+          };
+        }
+        return window;
+      });
+
+      state.expandedWindows = filteredWindowsOptions;
+    },
+  },
+});
+
+export const {
+  openWindow,
+  closeWindow,
+  toFrontWindow,
+  minimizeWindow,
+  fullscreenWindow,
+} = windowsSlice.actions;
+
+export default windowsSlice.reducer;
